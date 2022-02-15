37d894d342e87952d83fd35564cf81f6bd3927ef
Shweta Shendage <shwetashendage@gmail.com>
2022-02-15 15:59:57 +0530

    Initial project setup

---
+++ PatternFramework/PatternFramework.h
@@ -0,0 +1,18 @@
+//
+//  PatternFramework.h
+//  PatternFramework
+//
+//  Created by Shweta Shendage on 15/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PatternFramework.
+FOUNDATION_EXPORT double PatternFrameworkVersionNumber;
+
+//! Project version string for PatternFramework.
+FOUNDATION_EXPORT const unsigned char PatternFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PatternFramework/PublicHeader.h>
+
+
