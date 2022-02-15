b66cf885013f13822f90190c9f9b9d698107dccf
Shweta Shendage <shwetashendage@gmail.com>
2022-02-15 16:17:09 +0530

    Added test cases

---
+++ PatternFramework/Pattern.swift
@@ -0,0 +1,21 @@
+//
+//  Pattern.swift
+//  PatternFramework
+//
+//  Created by Shweta Shendage on 15/02/22.
+//
+
+import Foundation
+public final class Pattern {
+
+    let name = "Pattern"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- PatternFrameworkTests/PatternFrameworkTests.swift
+++ PatternFrameworkTests/PatternFrameworkTests.swift
@@ -9,25 +9,20 @@ import XCTest
 @testable import PatternFramework
 
 class PatternFrameworkTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var pattern: Pattern!
+    
+    override func setUp() {
+        pattern = Pattern()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testAdd() {
+        XCTAssertEqual(pattern.add(a: 1, b: 1), 2)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSub() {
+        XCTAssertEqual(pattern.sub(a: 2, b: 1), 1)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
+    
 }
